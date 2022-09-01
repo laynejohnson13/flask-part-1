44007bf5eca764032be5b259227aee313f7a668c
laynejohnson13 <laynejohnson13001@gmail.com>
2022-09-01 12:22:27 -0400

    edit

--- app.py
+++ app.py
@@ -3,8 +3,24 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+
+# index page created
+@app.route('/index/')
+
+# hell world phrase created
 def hello():
     return '<h1>Hello, World!</h1>'
 
+@app.route('/about/')
+def about():
+    return '<h3>About Us.</h3>'
+
+@app.route('/contact/')
+def contact():
+    return '<h3>To contact, please email Layne.Johnson@stonybrook.edu.</h3>'
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
+
